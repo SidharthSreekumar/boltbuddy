6410ad917c1ca79d1a2bb09333be7e2dfcad5386
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-03-07 01:03:33 -0500

    Renamed app to boltbuddy and installed pwa packs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Lightning
+# Bolt⚡Buddy
 
-A PWA app that calculates the distance at which a lightning strikes based on the delay between the lightning and the thunder. 
+Bolt⚡Buddy is a PWA app that calculates the distance at which a lightning strikes based on the delay between the lightning and the thunder. 
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.10.
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
